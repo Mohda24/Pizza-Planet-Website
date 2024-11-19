f6afb652592aeb5987bc81ed037a447cec76fa8f
Ouzed <mhamed.ouzed@edu.uiz.ac.ma>
2024-11-19 18:11:11 +0100

    add robots.txt

--- src/Components/shared/Seo/Seo.jsx
+++ src/Components/shared/Seo/Seo.jsx
@@ -6,7 +6,6 @@ function Seo({ title, description, keywords, type}) {
             <title>{title}</title>
             <meta name="description" content={description} />
             <meta name="keywords" content={keywords} />
-            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             <meta http-equiv="X-UA-Compatible" content="ie=edge" />
 
 
