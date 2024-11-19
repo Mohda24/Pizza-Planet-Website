03e7dc845801378ac26259258b27242ee15388ff
Ouzed <mhamed.ouzed@edu.uiz.ac.ma>
2024-11-19 15:58:15 +0100

    fix a problem with console

--- src/Components/shared/Form/Form.jsx
+++ src/Components/shared/Form/Form.jsx
@@ -7,17 +7,17 @@ function Form({addisClass=false}) {
         <form className={`${addisClass ? s[addisClass] :""} ${s.form}`} action="#">
             <div className={s.formEmail}>
                 <label htmlFor="email">بريدك الإلكتروني</label>
-                <input type="email" name='email' placeholder='name@gmail.com' required />
+                <input id='email' type="email" name='email' placeholder='name@gmail.com' autocomplete="on" required />
             </div>
 
             <div className={s.formSubject}>
                 <label htmlFor="subject">الموضوع</label>
-                <input type="text" name='Subject' placeholder='أخبرنا كيف يمكننا مساعدتك' required />
+                <input id='subject' type="text" name='Subject' autocomplete="on" placeholder='أخبرنا كيف يمكننا مساعدتك' required />
             </div>
 
             <div className={s.formMessage}>
                 <label htmlFor="Message">رسالتك</label>
-                <textarea name="Message" rows={6} placeholder='اترك تعليقًا...'></textarea>
+                <textarea id='Message' name="Message" rows={6} autocomplete="off" placeholder='اترك تعليقًا...'></textarea>
 
             </div>
             <Button text={"إرسال الرسالة"}/>
--- src/Routes/AppRoute.jsx
+++ src/Routes/AppRoute.jsx
@@ -1,26 +1,32 @@
 import React from 'react'
-import { createBrowserRouter,createRoutesFromChildren,Route,RouterProvider, } from 'react-router-dom'
+import { createBrowserRouter, createRoutesFromChildren, Route, RouterProvider, } from 'react-router-dom'
 import RouteLayout from './RouteLayout'
-import {ROUTES} from './Route'
+import { ROUTES } from './Route'
 
 function AppRoute() {
-    const routes= createRoutesFromChildren(
+    const routes = createRoutesFromChildren(
 
-        
-        <Route path='/' element={<RouteLayout/>}>
-            {ROUTES.map((route,index)=>(
-                <Route key={index} path={route.path} element={route.element}/>
+
+        <Route path='/' element={<RouteLayout />}>
+            {ROUTES.map((route, index) => (
+                <Route key={index} path={route.path} element={route.element} />
             ))}
         </Route>
-        
+
     )
     const router = createBrowserRouter(routes);
     return (
-        <RouterProvider router={router}  future={{
+        <RouterProvider router={router} future={{
             v7_startTransition: true,
-            
-            
-          }}/>
+            v7_relativeSplatPath: true,
+            v7_normalizeFormMethod: true,
+            v7_fetcherPersist: true,
+            v7_partialHydration: true,
+            v7_skipActionStatusRevalidation: true,
+
+
+
+        }} />
     )
 }
 
