e175a5965b7ef06e7747b98c7749f68ff9ee7635
Ouzed <mhamed.ouzed@edu.uiz.ac.ma>
2024-11-19 23:51:44 +0100

    dd

--- src/Components/shared/Footer/SocialMedia/SocialMedia.jsx
+++ src/Components/shared/Footer/SocialMedia/SocialMedia.jsx
@@ -5,16 +5,16 @@ import {insta,facebook,github,linkden} from '../../../../Assets/Svg/svg'
 function SocialMedia() {
     return (
         <div className={s.SocialMedia}>
-            <a className="youtubeIcon" alt=" a link for socialMedia Account">
+            <a className="youtubeIcon" alt=" a link for socialMedia Account" href='#' >
                 <img src={insta} alt=" Icon for instagram" />
             </a>
-            <a className="linkdenIcon" alt=" a link for socialMedia Account">
+            <a className="linkdenIcon" alt=" a link for socialMedia Account" href='https://github.com/Mohda24'>
                 <img src={github} alt=" Icon for github" />
             </a>
-            <a className="InstaIcon" alt=" a link for socialMedia Account">
+            <a className="InstaIcon" alt=" a link for socialMedia Account" href='#'>
                 <img src={linkden} alt=" Icon for linkden" />
             </a>
-            <a className="youtubeIcon" alt=" a link for socialMedia Account">
+            <a className="youtubeIcon" alt=" a link for socialMedia Account"  href='https://web.facebook.com/profile.php?id=100018567512305'>
                 <img src={facebook} alt=" Icon for Facebook" />
             </a>
             
